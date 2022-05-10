2490dd48e77a96b30b42b931ac2a0b20f2327205
Arif Can Üçer <arif3er@gmail.com>
2022-05-10 19:37:23 +0300

    CharacterController güncellendi
    Health ve movepseed eklendi

--- Everlasting/Assets/Scripts/CharacterController.cs
+++ Everlasting/Assets/Scripts/CharacterController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private double moveSpeed;
+    private double health;
+
     // Start is called before the first frame update
     void Start()
     {
