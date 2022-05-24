28fd234df1a6c41c1ea6c7e39562d3ae1f00a597
Arif Can Üçer <arif3er@gmail.com>
2022-05-24 14:21:22 +0300

    Flicker

--- EverlastingGameProject/Assets/2 - Scripts/Objects/FlickeringLight.cs
+++ EverlastingGameProject/Assets/2 - Scripts/Objects/FlickeringLight.cs
@@ -5,5 +5,53 @@ using UnityEngine.Rendering.Universal;
 
 public class FlickeringLight : MonoBehaviour
 {
-   
+
+    private Light2D myLight;
+
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float minIntensity;
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float maxIntensity;
+
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float minInnerRadius;
+    [Range(0f, 20f)]
+    [SerializeField]
+    private float maxInnerRadius;
+
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float minOuterRadius;
+    [Range(0f, 10f)]
+    [SerializeField]
+    private float maxOuterRadius;
+
+    [Range(0f, 1f)]
+    [SerializeField]
+    private float minFalloffStrength;
+    [Range(0f, 1f)]
+    [SerializeField]
+    private float maxFalloffStrength;
+
+
+    private void Start()
+    {
+        myLight = GetComponent<Light2D>();
+    }
+
+    private void Flicker()
+    {
+        myLight.intensity = Random.Range(minIntensity, maxIntensity);
+        myLight.pointLightInnerRadius = Random.Range(minInnerRadius, maxInnerRadius);
+    }
+
+
+
+
+
+
+
 }
