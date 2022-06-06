28337ec4dafc8598d976f6e5a1fadcf567741c69
Arif Can Üçer <arif3er@gmail.com>
2022-06-06 23:59:10 +0300

    son dkler

---
+++ EverlastingGameProject/Assets/EndMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+
+        Application.Quit();
+    }
+}
